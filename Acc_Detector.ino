cf931a5c8506ed2a4dd2b7a07a5a7fd5d0750c70
vkdlsvnt <vkdlsvnt@naver.com>
2017-11-16 18:03:29 +0900

    Source Code Modified
    - Vector3 class feature enhancement
    - MPU9250 IMU support

--- Acc_Detector.ino
+++ Acc_Detector.ino
@@ -1,12 +1,23 @@
 #include <SoftwareSerial.h>
+#include "MPU9250.h"
+#include "I2Cdev.h"
+#include "pin_definition.h"
 
 // 이 선언이 있으면 부저가 울리기 시작한 1초 뒤에 자동으로 부저가 꺼집니다.
 //#define ALARM_AUTO_OFF
 
+// 이 선언이 있으면 가속도 센서 입력에 Low Pass Filter가 적용됩니다.
+#define ENABLE_ACC_LOW_PASS
+#if defined(ENABLE_ACC_LOW_PASS)
+#define ts 0.03
+#define tau 1/(0.8*2*3.14)
+#define ttt ts/(tau+ts)
+#endif
+
 void alert();
 void non_alert();
 
-typedef int16_t vt;
+typedef float vt;
 // Vector3 클래스
 // 3차원 벡터값을 관리하는 클래스입니다.
 class Vector3 {
@@ -16,53 +27,175 @@ private:
 	vt z = 0;
 
 public:
-	Vector3() {};
-	Vector3(vt x, vt y, vt z) {
-		this->x = x; this->y = y; this->z = z;
+	// constructor
+	Vector3() : x(0), y(0), z(0) {};
+	Vector3(const Vector3 &v) : x(v.x), y(v.y), z(v.z) {};
+	Vector3(const vt &_x, const vt &_y, const vt &_z) : x(_x), y(_y), z(_z) {};
+
+
+	// setter
+
+	void set(const Vector3 &v) { x = v.x; y = v.y; z = v.z; }
+	void set(const vt &_x, const vt &_y, const vt &_z) { x = _x; y = _y; z = _z; }
+	void setX(const vt &_x) { x = _x; }
+	void setY(const vt &_y) { y = _y; }
+	void setZ(const vt &_z) { z = _z; }
+	void setdX(const vt &_x) { x += _x; }
+	void setdY(const vt &_y) { y += _y; }
+	void setdZ(const vt &_z) { z += _z; }
+
+
+	// getter
+
+	vt getX() { return x; }
+	vt getY() { return y; }
+	vt getZ() { return z; }
+	// 벡터의 크기를 구합니다.
+	float getMagnitude() { return sqrt(x*x + y*y + z*z); }
+
+	// 벡터의 단위 벡터를 구합니다.
+	Vector3 getDirection() { return *this / getMagnitude(); }
+
+	// x축과 벡터 사이의 각도의 cos 값을 구합니다.
+	float getDirCosAlpha() { return (float)x / getMagnitude(); }
+
+	// y축과 벡터 사이의 각도의 cos 값을 구합니다.
+	float getDirCosBeta() { return (float)y / getMagnitude(); }
+
+	// z축과 벡터 사이의 각도의 cos 값을 구합니다.
+	float getDirCosGamma() { return (float)z / getMagnitude(); }
+
+
+	// 벡터 연산
+
+	Vector3 operator+(const Vector3 &v) { return Vector3(x + v.x, y + v.y, z + v.z); }
+	Vector3 operator-(const Vector3 &v) { return Vector3(x - v.x, y - v.y, z - v.z); }
+
+	Vector3 operator+(const vt &d) { return Vector3(x + d, y + d, z + d); }
+	Vector3 operator-(const vt &d) { return Vector3(x - d, y - d, z - d); }
+
+	void operator+=(const Vector3 &v) { x += v.x; y += v.y; z += v.z; }
+	void operator-=(const Vector3 &v) { x -= v.x; y -= v.y; z -= v.z; }
+
+	void operator+=(const vt &d) { x += d; y += d; z += d; }
+	void operator-=(const vt &d) { x -= d; y -= d; z -= d; }
+
+	Vector3 operator*(const vt &d) { return Vector3(x*d, y*d, z*d); }
+	Vector3 operator/(const vt &d) { return Vector3(x / d, y / d, z / d); }
+
+	void operator*=(const vt &d) { x *= d; y *= d; z *= d; }
+	void operator/=(const vt &d) { x /= d; y /= d; z /= d; }
+
+	vt dotTo(const Vector3 &v) { return x*v.x + y*v.y + z*v.z; }
+	Vector3 crossTo(const Vector3 &v) { return Vector3(y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x); }
+
+
+	// Serial print
+	void printxyz() {
+		Serial.print(x);
+		Serial.print(' ');
+		Serial.print(y);
+		Serial.print(' ');
+		Serial.print(z);
 	}
 
-	void setxyz(vt x, vt y, vt z) {
-		this->x = x; this->y = y; this->z = z;
+	void printxyzln() { printxyz(); Serial.println(); }
+
+	void printcos() {
+		Serial.print(getDirCosAlpha());
+		Serial.print(' ');
+		Serial.print(getDirCosBeta());
+		Serial.print(' ');
+		Serial.print(getDirCosGamma());
 	}
 
-	void setxyz(Vector3 v) {
-		x = v.x; y = v.y; z = v.z;
-	}
 
-	vt getAbs() {
-		return sqrt((uint32_t)x*x + (uint32_t)y*y + (uint32_t)z*z);
-	}
-	Vector3 getDir() {
-		vt abs = getAbs();
-		return Vector3(x / abs, y / abs, z / abs);
-	}
-	Vector3 getxyz() { return Vector3(x, y, z); }
-	inline vt getx() { return x; }
-	inline vt gety() { return y; }
-	inline vt getz() { return z; }
+	// destructor(auto)
+	// ~Vector3();
 };
 
-// Acc 클래스
-// 가속도 센서와 측정값을 관리하는 클래스입니다.
-class Acc {
+
+enum SENSOR {
+	ACCEL = 1,
+	GYRO = 2,
+	MAGNETRO = 4
+};
+
+// GY9250 클래스
+// 9축(가속도3축, 자이로3축, 자기장3축) 센서를 관리하는 클래스입니다.
+class GY9250 {
 private:
-	Vector3 raw;
-	uint8_t pin[3] = { A2, A3, A4 };
+	Vector3 a;
+	Vector3 ba;
+	Vector3 init_a;
+	int16_t buffer[9] = { 0 };
+	
+	MPU9250 M;
+
+	uint8_t sensorEnabled = 0x00;
+
+
 public:
-	void update() { raw.setxyz(analogRead(A0), analogRead(A1), analogRead(A2)); }
-	Vector3 getDir() { return raw.getDir(); }
-	vt getAbs() { return raw.getAbs(); }
-	Vector3 getRaw() { return raw.getxyz(); }
-	inline vt getx() { return raw.getx(); }
-	inline vt gety() { return raw.gety(); }
-	inline vt getz() { return raw.getz(); }
-} Acc;
+	int block = 1;
+	// constructor
+	/*GY9250() {
+	}*/
+
+	bool init() {
+		Serial.println(F("GY9250 initializing...."));
+		M.initialize();
+		delay(1000);
+		Serial.print(F("Device ID: ")); Serial.println(M.getDeviceID(), HEX);
+		//return M.testConnection();
+		return 1;
+	}
+
+	// 센서 선택
+	uint8_t activate(uint8_t en) {
+		sensorEnabled = en;
+		if (sensorEnabled&ACCEL) {
+			M.setStandbyXAccelEnabled(0);
+			M.setStandbyYAccelEnabled(0);
+			M.setStandbyZAccelEnabled(0);
+		}
+		Serial.print(F("Sensor enabled: ")); Serial.print(sensorEnabled&ACCEL); Serial.println(sensorEnabled&GYRO);
+		return sensorEnabled;
+	}
+
+	// enable 된 센서의 값을 읽고 업데이트합니다.
+	void updateData(void) {
+		M.getAcceleration(buffer, buffer + 1, buffer + 2);
+		if (sensorEnabled&ACCEL) {
+			/*a.set(fmap(buffer[0], 18750, -13950, 9.8, -9.8), fmap(buffer[1], 17800, -15000, 9.8, -9.8), fmap(buffer[2], 12875, -20550, 9.8, -9.8));*/// a /= 16384;
+			a.set(buffer[0], buffer[1], buffer[2]);
+			a = ba + (a - ba)*ttt;
+			ba = a;
+		}
+	}
+
+	// Serial prints
+	void printAccelerometer() {
+		/*Serial.print(a.getMagnitude());*/
+		Serial.print(a.getX());
+		Serial.print(' ');
+		Serial.print(a.getY());
+		Serial.print(' ');
+		Serial.print(a.getZ());
+		Serial.print(' ');
+		Serial.print(a.getMagnitude());
+	}
+
+	Vector3 getA() {
+		return a;
+	}
+
+} GY9250;
 
 
 // Buzzer 클래스
 // 부저의 소리를 관리하는 클래스입니다.
 // 선언과 동시에 초기화되어 있어서 따로 초기화해줄 필요는 없습니다.
-// alarm.on() 함수로 부저를 켜고
+// alarm.on() 함수로 부저를 켜고 
 // alarm.off() 함수로 부저를 끌 수 있습니다.
 // 클래스 선언 마지막 부분에 클래스 초기화 2번째 인자의 숫자를 바꾸어 주파수를 바꿀 수 있습니다. (기본: 1000)
 class Buzzer {
@@ -84,19 +217,19 @@ public:
 #endif
 	}
 	void off() { noTone(pin); Serial.println("alarm off"); }
-} alarm(12, 1000);
+} alarm(PIN_BUZZER, 1000);
 
 // Pressure 클래스
 // 압력센서를 관리하는 클래스입니다.
 // 선언과 동시에 초기화되어서 따로 초기화해줄 필요는 없습니다.
 class Pressure {
 private:
-	uint8_t pin = A5;
+	uint8_t pin = PIN_PRESS;
 	uint16_t threshold;
 
 public:
 	Pressure() {
-		pinMode(A5, INPUT_PULLUP);
+		pinMode(pin, INPUT_PULLUP);
 	}
 	bool isPressed() {
 		return threshold < analogRead(pin);
@@ -134,6 +267,7 @@ public:
 	Detector(uint16_t t, void(*func1)(void), void(*func2)(void)) : threshold(t) {
 		callback1 = func1;
 		callback2 = func2;
+#if defined(__AVR_ATmega328P__)
 		cli();
 		TCCR1A = 0;
 		TCCR1B = 0;
@@ -143,11 +277,13 @@ public:
 		TCCR1B |= (1 << CS10);
 		TIMSK1 &= 0xFD;
 		sei();
+#else
+	#error "Detector service not designed to target microcontroller."
+#endif
 	}
 	void on() {
 		TIMSK1 |= (1 << OCIE1A);
-		for (int i = 0; i <= 3; i++) Acc.update();
-		initV.setxyz(Acc.getRaw());
+		initV = GY9250.getA();
 		Pressure.setThreshold();
 		s = 1;
 	}
@@ -167,32 +303,31 @@ ISR(TIMER1_COMPA_vect) {
 	isr_now_time = millis();
 	if (isr_now_time - isr_old_time > isr_interval) {
 		isr_old_time = isr_now_time;
-		Acc.update();
-		now = Acc.getRaw();
-		Serial.print(Detector.initV.getx() - now.getx());
+		GY9250.updateData();
+		now = GY9250.getA();
+		Serial.print(Detector.initV.getX() - now.getX());
 		Serial.print('\t');
-		Serial.print(Detector.initV.gety() - now.gety());
+		Serial.print(Detector.initV.getY() - now.getY());
 		Serial.print('\t');
-		Serial.print(Detector.initV.getz() - now.getz());
+		Serial.print(Detector.initV.getZ() - now.getZ());
 		Serial.print('\t');
-		Serial.print(vt(Detector.initV.getAbs() - now.getAbs()));
+		Serial.print(vt(Detector.initV.getMagnitude() - now.getMagnitude()));
 		Serial.print('\t');
-		Serial.print(now.getAbs());
+		Serial.print(now.getMagnitude());
 		Serial.print('\t');
 		Serial.println(analogRead(A5));
-		if (abs(Detector.initV.getx() - now.getx())>Detector.threshold || abs(Detector.initV.getz() - now.getz())>Detector.threshold || Pressure.isPressed()) Detector.callback1();
+		if (abs(Detector.initV.getX() - now.getX())>Detector.threshold || abs(Detector.initV.getZ() - now.getZ())>Detector.threshold || Pressure.isPressed()) Detector.callback1();
 		else Detector.callback2();
 	}
 }
 
-SoftwareSerial BT(4, 5);
+SoftwareSerial BT(PIN_BT_RX, PIN_BT_TX);
 
 // 움직임 감지 모드가 ON인 상태에서 움직임이 감지될 때 실행할 코드를
 // alert() 함수 안에 작성하세요.
 // delay 함수 사용은 자제하도록 합니다.
 void alert() {
 	alarm.on();
-	BT.print('A');
 }
 
 // 움직임 감지 모드에서 움직임이 감지되지 않은 상태일 때 실행할 코드를
@@ -204,11 +339,17 @@ void non_alert() {
 
 // 버튼이 눌릴 때 실행할 코드를
 // button() 함수 안에 작성하세요.
+// delay 함수 사용은 자제하도록 합니다.
 void button() {
 	alarm.off();
 	Detector.on();
 }
 
+// float mapping function
+float fmap(float x, float in_min, float in_max, float out_min, float out_max)
+{
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 
 void setup() {
@@ -216,7 +357,7 @@ void setup() {
 	BT.begin(9600);
 	pinMode(13, OUTPUT);
 	pinMode(2, INPUT_PULLUP);
-	attachInterrupt(digitalPinToInterrupt(2), button, FALLING);
+	attachInterrupt(digitalPinToInterrupt(PIN_INT_BTN), button, FALLING);
 	delay(100);
 
 	// 기타 초기에 한 번 실행할 코드를 아래에 작성하세요.
---
+++ pin_definition.h
@@ -0,0 +1,5 @@
+#define PIN_BUZZER 12
+#define PIN_PRESS A5
+#define PIN_BT_RX 4
+#define PIN_BT_TX 5
+#define PIN_INT_BTN 2
